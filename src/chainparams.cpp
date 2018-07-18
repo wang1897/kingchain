9969fdf56e652f42f9810642ca3a727b71fd7055
wang1897 <wk1897206@163.com>
2018-07-18 12:45:19 +0800

    firstCommit

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -387,7 +387,7 @@ public:
     {
         strNetworkID = gArgs.GetArg("-chain", "");
         assert(strNetworkID.size() != 0);
-//        consensus.nSubsidyInit = atoi(gArgs.GetArg("-subsidy-init", "0"));
+//         consensus.nSubsidyInit = atoi(gArgs.GetArg("-subsidy-init", "0"));
         consensus.nSubsidyHalvingInterval = atoi(gArgs.GetArg("-subsidy-halving-interval", "0"));
         consensus.nSubsidyHalvingTime = atoi(gArgs.GetArg("-subsidy-halving-time", "7"));
         consensus.nSubsidyInit = atoi("50");
